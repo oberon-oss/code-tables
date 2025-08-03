956136ed780224f5270ad38bfe1b7059430d52f9
TigerLilly64 <info@oberon-oss.eu>
2025-08-04 00:11:11 +0200

    Initial commit

---
+++ src/main/java/eu/oberon/oss/tools/cc/CountryCodeTableException.java
@@ -0,0 +1,27 @@
+package eu.oberon.oss.tools.cc;
+
+/**
+ * Exception used by country code table code
+ *
+ * @author TigerLilly64
+ * @since 1.0.0
+ */
+public class CountryCodeTableException extends RuntimeException {
+    /**
+     * {@inheritDoc}
+     *
+     * @since 1.0.0
+     */
+    public CountryCodeTableException(String message) {
+        super(message);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @since 1.0.0
+     */
+    public CountryCodeTableException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/eu/oberon/oss/tools/cc/CountryCodeTableLookupKeys.java
@@ -0,0 +1,34 @@
+package eu.oberon.oss.tools.cc;
+
+/**
+ * Defines the available lookup key types to query a country code table.
+ *
+ * @author TigerLilly64
+ * @since 1.0.0
+ */
+public enum CountryCodeTableLookupKeys {
+    /**
+     * Represent the name of the country to lookup.
+     *
+     * @since 1.0.0
+     */
+    NAME,
+    /**
+     * Represent the 2-letter code of the country to lookup.
+     *
+     * @since 1.0.0
+     */
+    ISO3166_ALPHA_2,
+    /**
+     * Represent the 3-letter code of the country to lookup.
+     *
+     * @since 1.0.0
+     */
+    ISO3166_ALPHA_3,
+    /**
+     * Represents the ' Standard Country or Area Codes for Statistical Use' code of the country to lookup.
+     *
+     * @since 1.0.0
+     */
+    UNM49
+}
