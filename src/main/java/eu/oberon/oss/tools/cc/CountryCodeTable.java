288b0a0fcba2c33762f743f59d9c844e6a2266c3
Fabien H. Dumay <fabien.dumay@capgemini.com>
2026-04-16 15:12:48 +0200

    Changes made: - removed unused imports

--- src/main/java/eu/oberon/oss/tools/cc/CountryCodeTable.java
+++ src/main/java/eu/oberon/oss/tools/cc/CountryCodeTable.java
@@ -1,6 +1,5 @@
 package eu.oberon.oss.tools.cc;
 
-import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
